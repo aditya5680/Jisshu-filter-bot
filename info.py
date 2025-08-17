c6bf78f8bdee873dc5f06d272610ed45ff899867
aditya5680 <gautambgaikwad184@gmail.com>
2025-08-18 00:14:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "https://t.me/+Gt7X4PWT4yphMmE1")
 MOVIE_GROUP_LINK = environ.get("MOVIE_GROUP_LINK", "https://t.me/+zKjL4eLWdGYzZWQ1")
 
 # Verification
-IS_VERIFY = is_enabled("IS_VERIFY", True)
+IS_VERIFY = is_enabled("IS_VERIFY", False)
 # ---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/HowToUseBoTx/4")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/HowToUseBoTx/4")
